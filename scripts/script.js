80720215c9f939c16e2d75749a8f90c37675a693
Andrea <andbia@gmail.com>
2024-12-01 15:30:10 +1100

    first commit

---
+++ scripts/script.js
@@ -0,0 +1,23 @@
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('frontContainer'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: "./images/transparent/front.json" // lottie file path
+  })
+
+  var animation = bodymovin.loadAnimation({
+    container: document.getElementById('leftContainer'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: "./images/transparent/left.json" // lottie file path
+  })
+
+  var animation = bodymovin.loadAnimation({
+    container: document.getElementById('rightContainer'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: "./images/transparent/right.json" // lottie file path
+  })
